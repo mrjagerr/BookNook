8d8fa777e31a89c510c1ccce61f6e6da4e6bd83c
skyeler yearsley <s2krighty@gmail.com>
2023-10-26 11:02:58 -0500

    initial commit of api

---
+++ BookNookWebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookNookWebApi.Data
+{
+    public class ApplicationDbContext :DbContext
+
+
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { 
+        }
+    }
+}
